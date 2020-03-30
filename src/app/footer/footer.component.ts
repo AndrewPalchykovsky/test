8f414d5ea2dae42c0fc9cf030ae169e63c0203d2
Andrew Palchykovsky <dangerok2@gmail.com>
2020-03-30 22:17:37 +0300

    Scroll to top for footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -11,5 +11,15 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  scrollTop(event) {
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 5);
+  }
 
 }
