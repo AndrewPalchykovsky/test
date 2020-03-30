07ac89660562579ebbabf36e1732bee8c4048338
Andrew Palchykovsky <dangerok2@gmail.com>
2020-03-30 22:18:48 +0300

    Removed unnecessary code

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -11,11 +11,11 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  scrollTop(event) {
+  scrollTop() {
     const scrollToTop = window.setInterval(() => {
       const pos = window.pageYOffset;
       if (pos > 0) {
-        window.scrollTo(0, pos - 20); // how far to scroll on each step
+        window.scrollTo(0, pos - 20);
       } else {
         window.clearInterval(scrollToTop);
       }
