4a7aaae6b3ed804dcbf99df1986de6316c390eee
Andrew Palchykovsky <dangerok2@gmail.com>
2020-03-30 18:19:01 +0300

    Changed structure of service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ServicesHeroComponent } from './services/services-hero/services-hero.component';
 import { ServicesComponent } from './services/services.component';
-import { ServicesContentComponent } from './services/services-content/services-content.component';
+
 import { FooterComponent } from './footer/footer.component';
 import { CaseStudiesComponent } from './case-studies/case-studies.component';
 import { ExpertiseComponent } from './expertise/expertise.component';
@@ -16,9 +15,7 @@ import { AboutComponent } from './about/about.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    ServicesHeroComponent,
     ServicesComponent,
-    ServicesContentComponent,
     FooterComponent,
     CaseStudiesComponent,
     ExpertiseComponent,
