aa33da2dfc6c4dc8b65b5d701496afdaf49963fa
Andrew Palchykovsky <dangerok2@gmail.com>
2020-04-01 10:51:27 +0300

    Added JSON file instead json object

--- src/app/services/services-all/services-all.component.ts
+++ src/app/services/services-all/services-all.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Service } from './service.model';
+import * as data from './service-all.json';
 
 @Component({
   selector: 'app-services-all',
@@ -7,44 +7,7 @@ import { Service } from './service.model';
   styleUrls: ['./services-all.component.sass']
 })
 export class ServicesAllComponent implements OnInit {
-
-  services: Service[] = [
-    {
-      img: '../../assets/img/services-apps.png',
-      title: 'Mobile Apps',
-      // tslint:disable-next-line:max-line-length
-      content: 'We are a nimble team of designers, developers, and strategists who are passionate about helping our clients achieve their goals and grow their brands.',
-      link: '/mobile-apps'
-    },
-    {
-      img: '../../assets/img/services-webservices.png',
-      title: 'Web Services',
-      // tslint:disable-next-line:max-line-length
-      content: 'We are a nimble team of designers, developers, and strategists who are passionate about helping our clients achieve their goals and grow their brands.',
-      link: '/web-services'
-    },
-    {
-      img: '../../assets/img/services-augmented-reality.png',
-      title: 'Augmented Reality',
-      // tslint:disable-next-line:max-line-length
-      content: 'We are a nimble team of designers, developers, and strategists who are passionate about helping our clients achieve their goals and grow their brands.',
-      link: '/augmented-reality'
-    },
-    {
-      img: '../../assets/img/services-design.png',
-      title: 'UX/UI Design',
-      // tslint:disable-next-line:max-line-length
-      content: 'We are a nimble team of designers, developers, and strategists who are passionate about helping our clients achieve their goals and grow their brands.',
-      link: '/ux-ui-design'
-    },
-    {
-      img: '../../assets/img/services-development.png',
-      title: 'Development',
-      // tslint:disable-next-line:max-line-length
-      content: 'We are a nimble team of designers, developers, and strategists who are passionate about helping our clients achieve their goals and grow their brands.',
-      link: '/development'
-    },
-  ];
+  services: any = (data as any).default;
   constructor() { }
 
   ngOnInit(): void {
